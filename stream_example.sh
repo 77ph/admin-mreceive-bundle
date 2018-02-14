a0fe312d4a7937185f54784310419cf90d8f0042
77ph <33835429+77ph@users.noreply.github.com>
2018-02-14 17:39:07 +0300

    Create stream_example.sh

---
+++ stream_example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nohup tsplay /root/233.163.114.233_5466.txt 239.255.99.99:1234 -max 3370218 -loop &
+nohup tsplay /root/233.163.114.121_5242.ts 239.255.99.98:1234 -max 3370218 -loop &
+nohup tsplay /root/233.163.114.30_5060.txt 239.255.99.97:1234 -max 3370218 -loop &
+nohup tsplay /root/224.2.2.2_50001.txt 239.255.99.96:1234 -max 3370218 -loop &
+nohup tsplay /root/233.163.114.233_5466.txt 239.255.99.95:1234 -max 3370218 -loop &
